c59efa4a235be4d1f2561e1c9b2b90c0905a6b57
bertil291utn <bertilt4@gmail.com>
2019-10-22 23:34:21 -0500

    server dist project name

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/{{your-app-name}}'));
+app.use(express.static('./dist/boletosOnline'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/{{your-app-name}}/index.html'));
+res.sendFile(path.join(__dirname,'/dist/boletosOnline/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
